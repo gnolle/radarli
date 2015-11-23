c22909fd63e829eb206c0a6b7a5aaa8e9cb901ca
Jan Klostermann <j.klostermann@posteo.de>
2015-11-23 16:00:52 +0100

    Refactored Distance Calculator.

--- app/src/main/java/com/example/jan/butzradar/DistanceCalculator.java
+++ app/src/main/java/com/example/jan/butzradar/DistanceCalculator.java
@@ -4,13 +4,17 @@ package com.example.jan.butzradar;
 
 import android.util.JsonReader;
 
+import com.google.android.gms.maps.model.LatLng;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 
 /**
  * Created by janklostermann on 20.11.15.
@@ -18,10 +22,17 @@ import java.net.URL;
 public class DistanceCalculator {
 
     private static final String CLASS_ID = "DISTANCE";
+    private LatLng origin;
+    private LatLng destination;
+
+    public DistanceCalculator(LatLng origin, LatLng destination) {
+        this.origin = origin;
+        this.destination = destination;
+    }
 
     private void getDistance() {
         try {
-            URL url = new URL("https://maps.googleapis.com/maps/api/distancematrix/json?origins=Wolfsburg&destinations=Wernigerode&language=de-DE");
+            URL url = new URL(buildAPICall());
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setUseCaches(false);
             connection.setRequestProperty("Connection", "close");
@@ -51,6 +62,26 @@ public class DistanceCalculator {
         }
     }
 
+    private String buildAPICall() {
+        StringBuilder callURL = new StringBuilder();
+        callURL.append(encodeToURIFormat("https://maps.googleapis.com/maps/api/distancematrix/json?origins="));
+        callURL.append(encodeToURIFormat(String.valueOf(origin.latitude) + "," + String.valueOf(origin.longitude)));
+        callURL.append(encodeToURIFormat("&destinations="));
+        callURL.append(encodeToURIFormat(String.valueOf(destination.latitude) + "," + String.valueOf(destination.longitude)));
+        callURL.append(encodeToURIFormat("&language=de-DE"));
+
+        return callURL.toString();
+    }
+
+    public String encodeToURIFormat(String toEncode) {
+
+        try {
+            return URLEncoder.encode(toEncode, "UTF-8");
+        } catch(UnsupportedEncodingException uce) {
+            return toEncode;
+        }
+    }
+
     private void parseJSON(String data) {
         try {
             JSONObject jsonObject = new JSONObject(data);
@@ -61,7 +92,4 @@ public class DistanceCalculator {
         }
     }
 
-    public static void main(String[] args) {
-        new DistanceCalculator().getDistance();
-    }
 }
